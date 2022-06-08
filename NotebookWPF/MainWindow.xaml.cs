5df4ad447d513fe819ee0c343d19bf41e636ef85
mthsvva <mixail.larionov.2002@mail.ru>
2022-06-08 20:23:48 +0500

    Color theme changing has been added.

--- NotebookWPF/MainWindow.xaml.cs
+++ NotebookWPF/MainWindow.xaml.cs
@@ -25,6 +25,23 @@ namespace NotebookWPF
             InitializeComponent();
 
             DataContext = new NoteViewModel(new DefaultDialogService(), new JsonFileService());
+
+        }
+
+        private void lightThemeMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            var uri = new Uri("lightTheme.xaml", UriKind.Relative);
+            ResourceDictionary resourceDict = Application.LoadComponent(uri) as ResourceDictionary;
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(resourceDict);
+        }
+
+        private void darkThemeMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            var uri = new Uri("darkTheme.xaml", UriKind.Relative);
+            ResourceDictionary resourceDict = Application.LoadComponent(uri) as ResourceDictionary;
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(resourceDict);
         }
     }
 }
